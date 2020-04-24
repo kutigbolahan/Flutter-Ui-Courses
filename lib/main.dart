f895dd5b2506f06f04a62044e4be8cdc57654d6a
kuti gbolahan <kutigbolahan@ymail.com>
2020-04-24 23:28:53 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:courses/homepage.dart';
 import 'package:courses/secondpage.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(   DevicePreview(
+    builder: (context)=> MyApp(),
+    enabled: !kReleaseMode,
+    )
+   );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // builder: (context, widget)=> ResponsiveWrapper.builder(
+      //   BouncingScrollWrapper.builder(context, widget),
+      //   maxWidth:1200,
+      //   minWidth:450,
+      //   defaultScale: true,
+      // ),
      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
